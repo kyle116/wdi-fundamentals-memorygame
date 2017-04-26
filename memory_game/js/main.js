9e86dcfb6f95ee0fa9b8333f23f5211f38ac9f72
kyle116 <kyle116@yahoo.com>
2017-04-25 20:06:00 -0700

    Add files via upload

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,8 +1,16 @@
-console.log("Up and running!");
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
+var cards = ["queen", "queen", "king", "king"];
+var cardOne = cards[0];
+var cardsInPlay = [];
+cardsInPlay.push(cardOne);
+console.log(cardOne)
+var cardTwo = cards[2];
+cardsInPlay.push(cardTwo);
+console.log(cardTwo)
 
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardTwo);
+if (cardsInPlay.length === 2) {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		alert("You found a match!");
+	} else {
+		alert("Sorry, try again.");
+	}
+}
